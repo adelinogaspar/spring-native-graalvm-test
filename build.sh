3b7ca82315aa9482aaa4cefd4203e8b6d744b781
Gaspar <adelinogaspar@gmail.com>
2025-06-17 18:31:49 -0300

    build adjust

--- build.sh
+++ build.sh
@@ -2,8 +2,5 @@
 --builder paketobuildpacks/builder:tiny \
 --env BP_NATIVE_IMAGE=true \
 --env BP_JAVA_VERSION=17 \
---env BP_NATIVE_IMAGE_BUILD_ARGUMENTS="--no-fallback" 
+--env BP_NATIVE_IMAGE_BUILD_ARGUMENTS="--no-fallback" \
 --verbose
-
-#--buildpack paketo-buildpacks/bellsoft-liberica \
-#--buildpack paketo-buildpacks/java-native-image \
